4dab576f5dd8e960926a803b25d8e3c899c31cf0
jkk@gmail.com <jkk@gmail.com>
2021-01-13 19:27:01 +0530

    reuse

---
+++ module/networking/vpc/variables.tf
@@ -0,0 +1,22 @@
+variable "environment" {
+    type = string
+}
+
+variable "domain_name_servers" {
+    type = list
+    default = [ "8.8.8.8" ]
+}
+
+variable "domain_name" {
+    type = string
+}
+
+variable "ntp_servers" {
+    type = list
+    default = [ "132.163.96.1", "132.163.96.2", "132.163.96.3", "132.163.96.4" ]
+}
+
+variable "vpc_cidr_block" {
+    type = string
+}
+
---
+++ prod/networking/variable.tf
@@ -0,0 +1,3 @@
+variable "region" {}
+variable "access_key" { sensitive = true }
+variable "secret_key" { sensitive = true }
